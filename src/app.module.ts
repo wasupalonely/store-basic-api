b744fbf1a0c40a322bb1b5817af10dd3d89c7435
wasupalonely <juandiegomaciasoy@gmail.com>
2022-08-21 19:06:13 -0500

    removing required from joi

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,9 +18,9 @@ import config from './config';
       load: [config],
       isGlobal: true,
       validationSchema: Joi.object({
-        API_KEY: Joi.number().required(),
-        DATABASE_NAME: Joi.string().required(),
-        DATABASE_PORT: Joi.number().required(),
+        API_KEY: Joi.number(),
+        DATABASE_NAME: Joi.string(),
+        DATABASE_PORT: Joi.number(),
       })
     }),
     UsersModule,
